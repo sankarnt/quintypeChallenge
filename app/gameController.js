8374047fd28facc4ee013dc03eff244d047323d3
sankar <sankarnt@yahoo.co.in>
2018-11-06 01:10:50 +0530

    fixed the functionality and responsieness

--- app/gameController.js
+++ app/gameController.js
@@ -22,27 +22,85 @@ app.controller("mineSweepController", function($scope){
 
         }
         $scope.hasDiamondsArr.sort(function(a, b){return a - b});
-        console.log($scope.hasDiamondsArr);      
     }
     $scope.getRandomObj = function() {
         var min = 0; 
         var max = 64;
-        return parseInt(Math.random() * (+max - +min) + +min); 
+        var randomInd = parseInt(Math.random() * (+max - +min) + +min); 
+        if($scope.hasDiamondsArr.indexOf(randomInd) > -1){
+            $scope.getRandomObj();
+        } else{
+            return randomInd;
+        }
     }
 
     $scope.revealPuzzle = function(box, elt){
         if(!!box && !!box.hasDiamond) {
-            elt.target.classList.add("mine-box-diamond-bg");
-            box.isRevealed = true;
-            // localStorage.setItem("game", JSON.stringify($scope.squares));
+            var indexToRemove = $scope.hasDiamondsArr.indexOf(box.index)
+            if(indexToRemove > -1) {
+                $scope.hasDiamondsArr.splice(indexToRemove, 1);
+                elt.target.classList.add("mine-box-diamond-bg");
+                box.isRevealed = true;
+            }
+            localStorage.setItem("game", JSON.stringify($scope.squares));
+            localStorage.setItem("diamond", JSON.stringify($scope.hasDiamondsArr));
+
+            if($scope.hasDiamondsArr.length === 0) {
+                alert("Yooooo!!! You Won!!!!");
+                localStorage.removeItem("game");
+                localStorage.removeItem("diamond");
+            }
         } else {
-            // elt.target.classList.add("mine-box-clue-bg");
-            
+            $scope.getClueDirection(box, elt);
         }
     }
+
     
+    $scope.prevClickedElt = null;
+    
+    $scope.getClueDirection = function(box, elt) {
+        var bIndex = box.index;
+        var minDifference;
+        var choosenIndex;
+        
+        if($scope.prevClickedElt) {
+            $scope.prevClickedElt.classList.remove("mine-box-clue-bg-left");
+            $scope.prevClickedElt.classList.remove("mine-box-clue-bg-right");
+        }
+        $scope.prevClickedElt = elt.target;
+        for (var i = 0; i < $scope.hasDiamondsArr.length; i++) {
+            var tempDifference = bIndex - $scope.hasDiamondsArr[i];
+            if(typeof(minDifference) === "undefined"){
+                minDifference = Math.abs(tempDifference);
+                choosenIndex = $scope.hasDiamondsArr[i];
+            } else if(Math.abs(tempDifference) < minDifference){
+                minDifference = Math.abs(tempDifference);
+                choosenIndex = $scope.hasDiamondsArr[i];
+            }
+        };
+
+        if(choosenIndex > bIndex) {
+            elt.target.classList.remove("mine-box-puzzle-bg");
+            elt.target.classList.add("mine-box-clue-bg-right");
+
+        } else if(choosenIndex < bIndex) {
+            elt.target.classList.remove("mine-box-puzzle-bg");
+            elt.target.classList.add("mine-box-clue-bg-left");
+        }
+    }
+
+    $scope.getBreakLine = function(index) {
+        if ((index + 1) % 8 == 1 && index > 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+
     if(localStorage.getItem("game")) {
         $scope.squares = JSON.parse(localStorage.getItem("game"));
+        $scope.hasDiamondsArr = JSON.parse(localStorage.getItem("diamond"));
     } else {
         $scope.createBlockObj();
     }
